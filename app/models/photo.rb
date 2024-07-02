062813298838ae92b42e6e482b8200bce94afd8a
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-07-02 20:22:59 +0000

    created sample_data task

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -22,7 +22,7 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
-  has_mamy :likes
+  has_many :likes
   has_many :fans, through: :likes
 
   validates :caption, presence: true
