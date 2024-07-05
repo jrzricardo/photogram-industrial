01ba9c22bf911a5a92b2822ab8c955fe708c940f
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-07-05 21:53:08 +0000

    Added  Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "photos#index"
+
+  #get "users/:id" => "users#show", as: :user
   
   devise_for :users
   
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
+  #resources :users, only: :show
+
+  get "/:username" => "users#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
