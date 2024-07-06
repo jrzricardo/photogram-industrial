b52f89b85132e5009d3e815488760756b5a84d60
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-07-06 20:35:49 +0000

    added remaining user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  #get "users/:id" => "users#show", as: :user
   
   devise_for :users
   
@@ -11,11 +10,10 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "users#liked", as: :liked
-
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
+  
   get ":username" => "users#show", as: :user
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
-
